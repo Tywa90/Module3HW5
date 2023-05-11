e7fbf68fb8d7a590d62cf616ef7c7a89399187bb
Serhii Amelin <amserega90@gmail.com>
2023-05-11 15:44:50 +0300

    add gitignore

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+
+namespace Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(new ThreadStart(Count));
+            //myThread.IsBackground = true;
+            myThread.Priority = ThreadPriority.Highest;
+            myThread.Start();
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Primary");
+                Console.WriteLine(i * i);
+                Thread.Sleep(300);
+            }
+        }
+
+        public static void Count()
+        {
+            Thread.Sleep(300);
+            for (int i = 0; i < 9; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Secondary");
+                Console.WriteLine(i * i);
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
